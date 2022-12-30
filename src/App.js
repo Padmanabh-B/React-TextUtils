92e1eef7c39d548a3addcd546acfc90895835208
Padmanabh Belavadi <pro.paddusonu@gmail.com>
2022-12-30 19:03:51 +0530

    added more functionalities in switching to darkmode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
-// import TextForm from './Components/TextForm';
+import TextForm from './Components/TextForm';
 
 function App() {
+
+  const [mode, setMode] = useState("light"); //Weather dark mode is enable or not
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#4d4d3d';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'light';
+    }
+  }
+
   return (
     <>
 
-      <Navbar title="TextUtils" aboutText="About" />
-      
-      <About />
+      <Navbar title="TextUtils" mode={ mode } toggleMode={ toggleMode } />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" mode={ mode } />
+      </div>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -21,22 +21,22 @@ export default function TextForm(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container" style={ { color: props.mode === 'dark' ? 'white' : 'black' } } >
                 <h1>{ props.heading }</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={ text } onChange={ handleOnChange } id="myBox" rows="6"></textarea>
+                    <textarea className="form-control" value={ text } onChange={ handleOnChange } style={ { backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black' } } id="myBox" rows="6"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={ handleUpClick }>Convert To Uppercase</button>
                 <button className="btn btn-danger mx-2" onClick={ handlelowerClick }>Convert To Uppercase</button>
                 <button className="btn btn-danger mx-2" onClick={ clearTextArea }>Clear The Area</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={ { color: props.mode === 'dark' ? 'white' : 'black' } } >
                 <h2>Your Text Summary</h2>
                 <p>{ text.split(" ").length } Words and { text.length } characters</p>
                 <p>{ 0.008 * text.split(" ").length } Minutes to read</p>
 
                 <h2>Preview</h2>
-                <p>{ text }</p>
+                <p>{ text.length > 0? text :"Enter Something to Preview it Here"}</p>
             </div>
         </>
     )
